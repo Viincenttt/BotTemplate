4c9ab910919417011d2238ec9fab1c023a4bbd8c
vincent kok <vincentkok@live.nl>
2021-03-01 19:08:09 +0100

    Initial commit

---
+++ src/BotTemplate.Application/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace BotTemplate.Application {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            Console.Read();
+        }
+    }
+}
