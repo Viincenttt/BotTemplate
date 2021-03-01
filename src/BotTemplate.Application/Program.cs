1d7cb305fc55a22e29fdc2bc6955a21f56aa0c86
vincent kok <vincentkok@live.nl>
2021-03-01 19:10:22 +0100

    Add commit with private nuget package

--- src/BotTemplate.Application/Program.cs
+++ src/BotTemplate.Application/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using TradingBot.Utilities.Collections;
 
 namespace BotTemplate.Application {
     class Program {
         static void Main(string[] args) {
+            var test = new CacheWithExpiry<string>(TimeSpan.FromMinutes(1));
+            test.AddOrUpdate("key", "value");
             Console.WriteLine("Hello World!");
             Console.Read();
         }
