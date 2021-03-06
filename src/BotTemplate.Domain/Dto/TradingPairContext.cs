a9ee669fb99d94c62d5cf2ceadba45897311a2bb
vincent kok <vincentkok@live.nl>
2021-03-06 21:16:25 +0100

    Trading works

--- src/BotTemplate.Domain/Dto/TradingPairContext.cs
+++ src/BotTemplate.Domain/Dto/TradingPairContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using BotTemplate.DAL.Entities;
 using TradingBot.Domain.Shared.Dto;
 using TradingBot.Exchange.Domain.Models.Response;
@@ -9,11 +10,14 @@ namespace BotTemplate.Domain.Dto {
     }
     
     public class TradingPairContext : ITradingPairContext {
+        private CandleStickResponse _latestCandleStick;
+        
         public Guid RunId { get; set; }
         public ConcreteTradingPair TradingPair { get; set; }
         public IEnumerable<CandleStickResponse> LatestCandleSticks { get; set; }
         public IEnumerable<CandleStickResponse> AllCandleSticks { get; set; }
-        public CandleStickResponse CurrentCandleStick { get; set; }
+        public CandleStickResponse CurrentCandleStick => this._latestCandleStick ??= this.LatestCandleSticks.LastOrDefault();
+
         public bool HasCandleSticks { get; set; }
     }
 }
